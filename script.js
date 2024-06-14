6e229b3f9582c0a7ca8cea4a83c151defe0d15f3
Mohamed El Dahtowry <salintofficial@hotmail.com>
2024-06-14 15:17:52 +0300

    Remove old site

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("scroll", (e) => {
-	if(window.scrollY >= window.innerHeight * 0.5) {
-		document.querySelector("header div#content").style.position = "absolute";
-	}
-	else {
-		document.querySelector("header div#content").style.position = "fixed";
-		document.querySelector("header div#content").style.top = "calc(100% - 100vh)";
-	}
-});
